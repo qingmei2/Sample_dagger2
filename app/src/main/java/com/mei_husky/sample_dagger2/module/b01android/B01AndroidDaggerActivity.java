94047c2b448a97f723db6a1096972e9b05979333
qing.mei <qing.mei@fenrir-inc.com.cn>
2017-07-28 18:38:09 +0800

    Add:B01Acitivity.class,The guide of Android-Dagger2

---
+++ app/src/main/java/com/mei_husky/sample_dagger2/module/b01android/B01AndroidDaggerActivity.java
@@ -0,0 +1,11 @@
+package com.mei_husky.sample_dagger2.module.b01android;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by QingMei on 2017/7/28.
+ * desc:
+ */
+
+public class B01AndroidDaggerActivity extends AppCompatActivity {
+}
